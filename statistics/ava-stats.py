0a7e4c664f549aaf5604081da981f950083b6ddc
Sewnkin <67289934+S3wnkin@users.noreply.github.com>
2020-06-25 00:05:29 +0300

    Update ava-stats.py
    Clearer code

--- statistics/ava-stats.py
+++ statistics/ava-stats.py
@@ -3,7 +3,7 @@ import hashlib
 import json
 from pprint import pprint
 from urllib3.exceptions import InsecureRequestWarning
-
+#===================[LOGO]=========================
 print('''
 
   ██████▓█████ █     ████▄    █ ██ ▄█▀██▓███▄    █ 
@@ -18,12 +18,19 @@ print('''
 	''')
 
 # put your email address in the email field & your password in the password field to get the FULL & hidden statistics about your profile, enjoy!
-#==========================================================
-Email = "@example@email.com"
+#==================[LOGIN INFO]======================
+Email = "example@email.com"
 Password = "your_password"
-#==========================================================
+#==================[SETTINGS]========================
+url = "https://api.avkn.co/auth/1/auth/1/login"
+url2 = "https://api.avkn.co/profile/1/profile/1/get"
+url3 = "https://api.avkn.co/ws/1/stat/1/get"
+url4 = "https://api.avkn.co/ws/1/xp/1/get"
+url5 = "https://api.avkn.co/ws/1/balance/1/get"
+timeout = 1000
+cert_verify = False
 req_payload = {}
-
+#===================[CONTENT]========================
 hash_object = hashlib.md5(Password.encode())
 md5_hash = hash_object.hexdigest()
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -31,20 +38,19 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 s = requests.Session()
 login_data = {"type":"email","request":{"password":md5_hash,"email_address":Email}}
 
-url = "https://api.avkn.co"
-r = s.post("https://api.avkn.co/auth/1/auth/1/login", json=login_data, timeout=1000, verify=False)
+r = s.post(url, json=login_data, timeout=timeout, verify=cert_verify)
 Logincookies = r.headers
 r.headers
-r = s.post("https://api.avkn.co/profile/1/profile/1/get", json=req_payload, headers=Logincookies, timeout=1000, verify=False)
+r = s.post(url2, json=req_payload, headers=Logincookies, timeout=timeout, verify=cert_verify)
 r = json.loads(r.text)
 
-r2 = s.post("https://api.avkn.co/ws/1/stat/1/get", json=req_payload, headers=Logincookies, timeout=1000, verify=False)
+r2 = s.post(url3, json=req_payload, headers=Logincookies, timeout=timeout, verify=cert_verify)
 r2 = json.loads(r2.text)
 
-r3 = s.post("https://api.avkn.co/ws/1/xp/1/get", json=req_payload, headers=Logincookies, timeout=1000, verify=False)
+r3 = s.post(url4, json=req_payload, headers=Logincookies, timeout=timeout, verify=cert_verify)
 r3 = json.loads(r3.text)
 
-r4 = s.post("https://api.avkn.co/ws/1/balance/1/get", json=req_payload, headers=Logincookies, timeout=1000, verify=False)
+r4 = s.post(url5, json=req_payload, headers=Logincookies, timeout=timeout, verify=cert_verify)
 r4 = json.loads(r4.text)
 
 class bcolors:
